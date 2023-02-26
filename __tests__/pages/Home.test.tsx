f806cd0539b206875bffc509b03cf66cfe9cdcd6
cubdesign <takeo@cubdesign.com>
2023-02-27 08:12:29 +0900

    add msw

--- __tests__/pages/Home.test.tsx
+++ __tests__/pages/Home.test.tsx
@@ -1,9 +1,15 @@
 import Home from "@/pages";
 import { describe, it } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 describe("Home", () => {
   it("renders a heading", async () => {
-    render(<Home />);
+    const queryClient = new QueryClient();
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    );
 
     const heading = screen.getByRole("heading", {
       name: /pokeapi sample/i,
