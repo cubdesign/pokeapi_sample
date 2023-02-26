a9e0f98365782598f8b3b113e39669199d5fb6c9
cubdesign <takeo@cubdesign.com>
2023-02-26 23:33:34 +0900

    husky test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Head>
         <title>pokeapi sample</title>
-        <meta name="description" content="pokeapi sample" />
+        <meta name="description" content="pokeapi sample." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
