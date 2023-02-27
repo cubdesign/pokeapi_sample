6d3a3250d027044149f7e950eed9895d30046a67
cubdesign <takeo@cubdesign.com>
2023-02-27 13:06:10 +0900

    add scrollRestoration

--- src/components/pokemon/PokemonRow.tsx
+++ src/components/pokemon/PokemonRow.tsx
@@ -1,5 +1,6 @@
 import { ApiGetPokemonResponse, getPokemon } from "@/services/pokeapi";
 import { Pokemon, PokemonDetails } from "@/types/pokemonTypes";
+import Box from "@mui/material/Box";
 import Link from "next/link";
 import { FC, useEffect, useState } from "react";
 import { useQuery } from "react-query";
@@ -7,36 +8,29 @@ export type PokemonRowProps = {
   pokemon: Pokemon;
 };
 const PokemonRow: FC<PokemonRowProps> = ({ pokemon }) => {
-  const [details, setDetails] = useState<PokemonDetails | null>(null);
-  const { data, isLoading } = useQuery<ApiGetPokemonResponse, Error>({
+  const { data: details, isLoading } = useQuery<ApiGetPokemonResponse, Error>({
     queryKey: [`pokemon`, pokemon.url],
     queryFn: () => getPokemon(pokemon.url),
   });
 
-  useEffect(() => {
-    if (data) {
-      setDetails({
-        id: data.id,
-        name: data.name,
-        imageURL: data.sprites.front_default,
-      });
-    }
-  }, [data]);
-
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div>
+    <Box
+      sx={{
+        height: "200px",
+      }}
+    >
       {details && (
         <Link href={`/pokemon/${details.id}`}>
           {/* eslint-disable @next/next/no-img-element */}
-          <img src={details.imageURL} alt={details.name} />
+          <img src={details.sprites.front_default} alt={details.name} />
           {/* eslint-enable @next/next/no-img-element */}
           <div>{pokemon.name}</div>
           <div>{details.id}</div>
         </Link>
       )}
-    </div>
+    </Box>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import { useQuery } from "react-query";
-import { useEffect, useState } from "react";
-import { Pokemon } from "@/types/pokemonTypes";
 import PokemonRow from "@/components/pokemon/PokemonRow";
 import { ApiGetPokemonsResponse, getPokemons } from "@/services/pokeapi";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 export default function Home() {
-  const limit = 10;
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const limit = 12;
+
   const { data, isLoading, error, isSuccess, isError, refetch } = useQuery<
     ApiGetPokemonsResponse,
     Error
@@ -16,11 +16,9 @@ export default function Home() {
     queryFn: () => getPokemons(limit),
   });
 
-  useEffect(() => {
-    if (data) {
-      setPokemons(data.results);
-    }
-  }, [data]);
+  const pokemons = data ? data.results : [];
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
@@ -29,10 +27,32 @@ export default function Home() {
         <meta name="description" content="pokeapi sample." />
       </Head>
       <main>
-        <h1>pokeapi sample</h1>
-        {pokemons.map((pokemon, index) => {
-          return <PokemonRow key={index} pokemon={pokemon}></PokemonRow>;
-        })}
+        <Container
+          sx={{
+            textAlign: "center",
+          }}
+        >
+          <h1>pokeapi sample</h1>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+            }}
+          >
+            {pokemons.map((pokemon, index) => {
+              return (
+                <Box
+                  key={index}
+                  sx={{
+                    width: "33%",
+                  }}
+                >
+                  <PokemonRow pokemon={pokemon}></PokemonRow>
+                </Box>
+              );
+            })}
+          </Box>
+        </Container>
       </main>
     </>
   );
